6e71408ecabd48ab37709e0b3151ab745ec178b4
Joshua Marvin Tabania <meliodas.demonheir@gmail.com>
2026-03-08 20:48:39 +0800

    last na talaga pramis

--- CustomerGarage/src/views/HomeView.vue
+++ CustomerGarage/src/views/HomeView.vue
@@ -151,6 +151,8 @@ onMounted(async () => {
                 <h3>Phone</h3>
                 <p>+63 956 556 7542</p>
                 <p>+63 948 485 4047</p>
+                <p>+63 955 231 4607</p>
+                <p>+63 966 411 8317</p>
                 <div class="social-links">
                   <span class="social-label">You may also connect with us via:</span>
                   <div class="social-buttons">
